2225d5419a12823cba3e80d12f44f2cc1090982d
matt <mattcollie@live.com>
2023-02-18 19:06:22 +1300

    adding links

---
+++ web/src/lib/Link.svelte
@@ -0,0 +1,27 @@
+<script>
+    export let href;
+    export let target = "_self";
+    let text;
+</script>
+
+<style>
+    a {
+        @apply
+            text-2xl
+            tracking-wider
+            hover:font-bold
+    }
+
+    a::before {
+        display: block;
+        content: attr(data-text);
+        font-weight: bold;
+        height: 0;
+        overflow: hidden;
+        visibility: hidden;
+    }
+</style>
+
+<a class="text-black dark:text-white" href={href} target={target} data-text={text? text.textContent : ""}>
+    <span bind:this={text}><slot/></span>
+</a>
--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,10 +1,18 @@
 <script>
     import "/src/app.css";
     import ThemeToggle from "$lib/ThemeToggle.svelte";
+    import Link from "$lib/Link.svelte";
 </script>
 
-<ThemeToggle />
-<div class="h-full w-full">
+<div class="flex flex-col items-start">
+    <ThemeToggle class="h-8" />
+    <div class="flex flex-row gap-12 py-36 px-64">
+        <nav class="flex flex-col gap-8 pt-24">
+            <Link href="/">home</Link>
+            <Link href="/about">about</Link>
+            <Link href="/blog">blog</Link>
+        </nav>
 
-    <slot/>
+        <slot/>
+    </div>
 </div>
