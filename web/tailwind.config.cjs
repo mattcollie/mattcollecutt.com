e7cbcad6bde02e197cfa4efb2498b4cd58828870
matt <mattcollie@live.com>
2023-02-18 15:45:44 +1300

    adding tailwind to web project

---
+++ web/tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        black: '#282828',
+        grey: '#767676',
+        white: '#F8F8F8'
+      }
+    },
+  },
+  plugins: [],
+}
