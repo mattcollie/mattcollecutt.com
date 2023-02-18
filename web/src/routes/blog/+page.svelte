1577511ab88d870bb10d4a1b20eabc5faa886f3e
matt <mattcollie@live.com>
2023-02-18 20:58:06 +1300

    installing mdsvex and using layouts

--- web/src/routes/blog/+page.svelte
+++ web/src/routes/blog/+page.svelte
@@ -1,22 +1,12 @@
 <script>
-    import { page } from "$lib/store";
     let blogPosts = [];
-    $page = "/blog";
 </script>
 
-<svelte:head>
-    <title>blog</title>
-</svelte:head>
-
-<h1>blog</h1>
-
-<div class="pt-12 flex flex-col gap-4">
-    {#if blogPosts.length > 0}
-        {#each blogPosts as {title, date}}
-            <h3>{title}</h3>
-            <p class="text-grey">{date}</p>
-        {/each}
-    {:else}
-        <p class="text-grey">Sorry, Can't find any posts at the moment!</p>
-    {/if}
-</div>
+{#if blogPosts.length > 0}
+    {#each blogPosts as {title, date}}
+        <h3>{title}</h3>
+        <p class="text-grey">{date}</p>
+    {/each}
+{:else}
+    <p class="text-grey">Sorry, Can't find any posts at the moment!</p>
+{/if}
--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from "svelte-preprocess";
+import {mdsvex} from "mdsvex";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,9 +11,13 @@ const config = {
 			fallback: 'index.html'
 		})
 	},
-	preprocess: [preprocess({
-		postcss: true
-	})]
+	extensions: ['.svelte', '.md'],
+	preprocess: [
+		preprocess({
+			postcss: true
+		}),
+		mdsvex({ extensions: ['.md'] })
+	]
 };
 
 export default config;
