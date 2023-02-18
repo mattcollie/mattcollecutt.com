b658b1ac02424ecb083cf34ecfd4596ce08453b9
matt <mattcollie@live.com>
2023-02-18 19:54:26 +1300

    adding blog page

--- web/src/routes/blog/+page.svelte
+++ web/src/routes/blog/+page.svelte
@@ -1,5 +1,18 @@
 <script>
     import { page } from "$lib/store";
-
-    $page = "/blog"
+    let blogPosts = [];
+    $page = "/blog";
 </script>
+
+<h1>blog</h1>
+
+<div class="pt-12 flex flex-col gap-4">
+    {#if blogPosts.length > 0}
+        {#each blogPosts as {title, date}}
+            <h3>{title}</h3>
+            <p class="text-grey">{date}</p>
+        {/each}
+    {:else}
+        <p class="text-grey">Sorry, Can't find any posts at the moment!</p>
+    {/if}
+</div>
