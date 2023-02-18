b73d49cddbf629c5c5150ac2de867c6dd82be2b7
matt <mattcollie@live.com>
2023-02-18 19:38:47 +1300

    adding general introduction

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -4,15 +4,17 @@
     import Link from "$lib/Link.svelte";
 </script>
 
-<div class="flex flex-col items-start">
+<div class="h-full flex flex-col items-start">
     <ThemeToggle class="h-8" />
-    <div class="flex flex-row gap-12 py-36 px-64">
+    <div class="h-full flex flex-row gap-12 py-36 px-64">
         <nav class="flex flex-col gap-8 pt-24">
             <Link href="/">home</Link>
             <Link href="/about">about</Link>
             <Link href="/blog">blog</Link>
         </nav>
 
-        <slot/>
+        <div class="h-full flex flex-col">
+            <slot/>
+        </div>
     </div>
 </div>
