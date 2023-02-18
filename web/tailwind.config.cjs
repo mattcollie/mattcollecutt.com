6fa7e3e06a5dd2b029dc53d6cd283630342614c1
matt <mattcollie@live.com>
2023-02-18 17:18:45 +1300

    adding font and setting up tailwind

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,14 +1,25 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
+  purge: [],
+  darkMode: 'class',
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {
-      colors: {
-        black: '#282828',
-        grey: '#767676',
-        white: '#F8F8F8'
+      fontFamily: {
+        serif: [
+          "garamond",
+          ...defaultTheme.fontFamily.serif
+        ]
       }
     },
+    colors: {
+      black: '#282828',
+      grey: '#767676',
+      white: '#F8F8F8'
+    }
   },
   plugins: [],
 }
