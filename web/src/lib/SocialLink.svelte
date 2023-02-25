54ba563298c544e87ea6d3a77a58809d050f0ded
matt <mattcollie@live.com>
2023-02-25 13:05:16 +1300

    adding link icons

---
+++ web/src/lib/SocialLink.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let icon;
+    export let name;
+    export let href;
+</script>
+
+<a href={href} target="_blank" rel="noreferrer">
+    <img src={icon} alt={name}>
+    {name}
+</a>
