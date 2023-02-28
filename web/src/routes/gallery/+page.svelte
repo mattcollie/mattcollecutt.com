d6bde4cd761cab630a553acc6645783476d0959c
matt <mattcollie@live.com>
2023-02-28 21:13:22 +1300

    adding photos and gallery

--- web/src/routes/gallery/+page.svelte
+++ web/src/routes/gallery/+page.svelte
@@ -1,6 +1,11 @@
 <script>
     import { page } from "$lib/store";
     $page = "/gallery";
+    export let data;
+    const {galleries} = data;
+    const galleryOrder = Object.keys(galleries).sort((a, b) => new Date(galleries[b].date) - new Date(galleries[a].date));
+
+    console.log(galleries, galleryOrder);
 </script>
 
 <svelte:head>
@@ -9,6 +14,43 @@
 
 <h1>Gallery</h1>
 
-<div class="flex flex-col gap-4 pt-10">
-    <p class="text-grey">Sorry, Can't find any photo galleries at the moment!</p>
+<div class="flex flex-row gap-4 pt-12">
+    {#if galleryOrder.length > 0}
+        {#each galleryOrder as key}
+            <div class="relative flex flex-col gap-4">
+                <div class="relative photo-container m-auto">
+                    {#each galleries[key].photos.slice(0, 3).reverse() as photo}
+                        <img src={photo} alt={photo} />
+                    {/each}
+                </div>
+                <p class="text-md text-black dark:text-white">{key}</p>
+            </div>
+        {/each}
+    {:else}
+        <p class="text-grey">Sorry, Can't find any photo galleries at the moment!</p>
+    {/if}
 </div>
+
+<style>
+    .photo-container {
+        width: 140px;
+        height: 140px;
+        position: relative;
+    }
+
+    img {
+        width: 128px;
+        height: 128px;
+        @apply
+            absolute m-auto object-cover
+            hover:transition hover:ease-in-out hover:scale-[1.02]
+    }
+    img:nth-child(2) {
+        top: 8px;
+        left: 8px;
+    }
+    img:nth-child(3) {
+        top: 16px;
+        left: 16px;
+    }
+</style>
