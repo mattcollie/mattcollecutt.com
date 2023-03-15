3e81d0f5e04ee937f01ad3e443471bc22b3b5647
matt <mattcollie@live.com>
2023-03-15 21:43:56 +1300

    fixing some svelte warnings

--- web/src/lib/socials/SocialLink.svelte
+++ web/src/lib/socials/SocialLink.svelte
@@ -10,18 +10,12 @@
             border border-2 rounded-lg p-2 text-xl font-bold align-middle
             hover:border-grey hover:text-grey
     }
-
-    svg {
-        @apply
-            border-black dark:border-white m-auto
-            hover:border-grey
-    }
 </style>
 
 <a class="border-black dark:border-white" href={href} target="_blank" rel="noreferrer">
     <slot />
     {name}
-    <svg width="14" height="14" viewBox="0 0 14 14" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    <svg class="border-black dark:border-white m-auto hover:border-grey" width="14" height="14" viewBox="0 0 14 14" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
         <path d="M12.7904 1L12.7904 9.38052M13 1.20951H4.6174M12.8548 1.14815L1 13" stroke="currentColor"/>
     </svg>
 </a>
