d2866d7f05606d3a58e2471df96d0e4329a77cbc
Mattc <matt@mustard.co.nz>
2023-11-28 20:29:22 +1300

    adding current works

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -2,6 +2,15 @@
     import Banner from "$lib/components/Banner.svelte";
 </script>
 
+<style>
+    a {
+        @apply
+            text-[#1F6FEB]
+            hover:underline
+        ;
+    }
+</style>
+
 <svelte:head>
     <title>Matt Collecutt</title>
     <meta name="description" content="Home page" />
@@ -10,6 +19,24 @@
 
 <Banner></Banner>
 
-<div class="mx-auto w-full max-w-3xl py-12">
-    content
+<div class="mx-auto w-full py-12 max-w-prose">
+    <article class="flex flex-col gap-5 text-sm font-light leading-6 tracking-wide max-w-prose">
+        <p>
+            Hi, I’m Matt! I'm a software developer, <a href="/photos">amateur photographer</a>,
+            <a href="/writing">writer</a>. Currently I'm working at
+            <a href="http://www.mustard.co.nz/" target="_blank">mustard technology</a>, a small development company
+            based in New Zealand.
+        </p>
+
+        <p>
+            Before Mustard, I worked at <a href="https://www.theaccessgroup.com/">Ezitracker</a>, where I started my
+            software development journey learning the ropes as an intern.
+        </p>
+
+        <p>
+            In my spare time I've been working on a few projects like <a href="https://mattcollecutt.co.nz">this</a>
+            website, <a href="https://trivino.xyz" target="_blank">TriVino</a> and
+            <a href="https://nookly.co.nz" target="_blank">Nookly</a>
+        </p>
+    </article>
 </div>
