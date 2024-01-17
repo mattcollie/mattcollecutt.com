fe396fbd33bda8ab14d681b6c6ddb4de400359d7
Mattc <matt@mustard.co.nz>
2024-01-18 00:36:40 +1300

    adding basic page, will refactor

--- gopi/main.go
+++ gopi/main.go
@@ -7,9 +7,10 @@ import (
 )
 
 func main() {
-	component := hello("Matt")
+	component := page("Matt Collecutt")
 
 	http.Handle("/", templ.Handler(component))
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 
 	fmt.Println("Listening on :3000")
 	http.ListenAndServe(":3000", nil)
