824fed7debf07111bd6d315dc30c5f7e4b5994d2
Mattc <matt@mustard.co.nz>
2024-01-20 23:40:04 +1300

    adding instagram images

--- gopi/main.go
+++ gopi/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/a-h/templ"
 	"log"
@@ -23,15 +24,42 @@ func main() {
 	}
 	homeComponent := HomePage(socialLinks)
 	http.Handle("/", templ.Handler(homeComponent))
-	http.Handle("/photos", templ.Handler(page("Photos", socialLinks)))
+	http.Handle("/photos", templ.Handler(PhotoPage(socialLinks)))
 	http.Handle("/writing", templ.Handler(page("Writing", socialLinks)))
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 	http.Handle("/robots.txt", http.FileServer(http.Dir("./static/")))
 
+	mediaResponse, err := media()
+	if err != nil {
+		log.Fatalf("Failed to get media: %v", err)
+	}
+	http.Handle("/instagram/media", templ.Handler(Photos(mediaResponse)))
+
 	addr := cfg.Server.Host + ":" + cfg.Server.Port
 	fmt.Println("Listening on " + addr)
-	err := http.ListenAndServe(addr, nil)
+	err = http.ListenAndServe(addr, nil)
 	if err != nil {
 		log.Fatalf("Fatal error failed to start server: %s \\n", err)
 	}
+}
+
+func media() ([]MediaItem, error) {
+	url := fmt.Sprintf("https://graph.instagram.com/%s/media?fields=caption,id,media_type,media_url,timestamp&access_token=%s", cfg.Instagram.UserId, cfg.Instagram.AccessToken)
+
+	response, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("error making request: %w", err)
+	}
+
+	if response.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("API request failed with status code %d", response.StatusCode)
+	}
+	defer response.Body.Close()
+
+	var myResponse MediaResponse
+	if err = json.NewDecoder(response.Body).Decode(&myResponse); err != nil {
+		return nil, fmt.Errorf("error decoding response: %w", err)
+	}
+
+	return myResponse.MediaItems, nil
 }
