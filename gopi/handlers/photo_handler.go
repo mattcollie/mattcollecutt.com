86b570b1b65b16d1459d4b80c3f4aaf5037e7c90
Mattc <matt@mustard.co.nz>
2024-01-21 19:50:42 +1300

    removing social links and moving media to models

--- gopi/handlers/photo_handler.go
+++ gopi/handlers/photo_handler.go
@@ -11,16 +11,11 @@ import (
 )
 
 func PhotoHandler(c *fiber.Ctx) error {
-	socialLinks := []models.SocialLink{
-		{Name: "Github", URL: "https://github.com/mattcollie", SVG: "/static/images/github.svg"},
-		{Name: "Linkedin", URL: "https://www.linkedin.com/in/matt-collecutt", SVG: "/static/images/linkedin.svg"},
-		{Name: "Instagram", URL: "https://www.instagram.com/matthewcollecutt", SVG: "/static/images/instagram.svg"},
-	}
 	mediaResponse, err := media()
 	if err != nil {
 		return err
 	}
-	return Render(c, templates.PhotoPage(socialLinks, mediaResponse))
+	return Render(c, templates.PhotoPage(mediaResponse))
 }
 
 func MediaHandler(c *fiber.Ctx) error {
@@ -40,45 +35,45 @@ func MediaAfterHandler(c *fiber.Ctx) error {
 	return Render(c, templates.Photos(mediaResponse))
 }
 
-func media() (templates.MediaResponse, error) {
+func media() (models.MediaResponse, error) {
 	limit := 12
 	url := fmt.Sprintf("https://graph.instagram.com/%s/media?fields=caption,id,media_type,media_url,timestamp,children{id,media_url}&access_token=%s&limit=%d", config.Cfg.Instagram.UserId, config.Cfg.Instagram.AccessToken, limit)
 
 	response, err := http.Get(url)
 	if err != nil {
-		return templates.MediaResponse{}, fmt.Errorf("error making request: %w", err)
+		return models.MediaResponse{}, fmt.Errorf("error making request: %w", err)
 	}
 
 	if response.StatusCode != http.StatusOK {
-		return templates.MediaResponse{}, fmt.Errorf("API request failed with status code %d", response.StatusCode)
+		return models.MediaResponse{}, fmt.Errorf("API request failed with status code %d", response.StatusCode)
 	}
 	defer response.Body.Close()
 
-	var myResponse templates.MediaResponse
+	var myResponse models.MediaResponse
 	if err = json.NewDecoder(response.Body).Decode(&myResponse); err != nil {
-		return templates.MediaResponse{}, fmt.Errorf("error decoding response: %w", err)
+		return models.MediaResponse{}, fmt.Errorf("error decoding response: %w", err)
 	}
 
 	return myResponse, nil
 }
 
-func mediaAfter(after string) (templates.MediaResponse, error) {
+func mediaAfter(after string) (models.MediaResponse, error) {
 	limit := 12
 	url := fmt.Sprintf("https://graph.instagram.com/%s/media?fields=caption,id,media_type,media_url,timestamp,children{id,media_url}&access_token=%s&limit=%d&after=%s", config.Cfg.Instagram.UserId, config.Cfg.Instagram.AccessToken, limit, after)
 
 	response, err := http.Get(url)
 	if err != nil {
-		return templates.MediaResponse{}, fmt.Errorf("error making request: %w", err)
+		return models.MediaResponse{}, fmt.Errorf("error making request: %w", err)
 	}
 
 	if response.StatusCode != http.StatusOK {
-		return templates.MediaResponse{}, fmt.Errorf("API request failed with status code %d", response.StatusCode)
+		return models.MediaResponse{}, fmt.Errorf("API request failed with status code %d", response.StatusCode)
 	}
 	defer response.Body.Close()
 
-	var myResponse templates.MediaResponse
+	var myResponse models.MediaResponse
 	if err = json.NewDecoder(response.Body).Decode(&myResponse); err != nil {
-		return templates.MediaResponse{}, fmt.Errorf("error decoding response: %w", err)
+		return models.MediaResponse{}, fmt.Errorf("error decoding response: %w", err)
 	}
 
 	return myResponse, nil
--- gopi/models/social_link.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type SocialLink struct {
-	Name string
-	URL  string
-	SVG  string
-}
