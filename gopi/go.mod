5c5c5241e8f74a3dccffe179a3d2a426fd8afca0
Mattc <matt@mustard.co.nz>
2024-01-21 18:31:53 +1300

    getting the child photos

--- gopi/go.mod
+++ gopi/go.mod
@@ -2,12 +2,26 @@ module gopi
 
 go 1.21
 
-require github.com/a-h/templ v0.2.513
+require (
+	github.com/a-h/templ v0.2.513
+	github.com/gofiber/fiber/v2 v2.52.0
+	github.com/ilyakaznacheev/cleanenv v1.5.0
+)
 
 require (
 	github.com/BurntSushi/toml v1.3.2 // indirect
-	github.com/ilyakaznacheev/cleanenv v1.5.0 // indirect
+	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/google/uuid v1.5.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/klauspost/compress v1.17.4 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.51.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
--- gopi/main.go
+++ gopi/main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/a-h/templ"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/adaptor"
 	"log"
 	"net/http"
 )
@@ -17,49 +19,112 @@ type SocialLink struct {
 func main() {
 	readConfig()
 
-	socialLinks := []SocialLink{
-		{name: "Github", url: "https://github.com/mattcollie", svg: "/static/images/github.svg"},
-		{name: "Linkedin", url: "https://www.linkedin.com/in/matt-collecutt", svg: "/static/images/linkedin.svg"},
-		{name: "Instagram", url: "https://www.instagram.com/matthewcollecutt", svg: "/static/images/instagram.svg"},
-	}
-	homeComponent := HomePage(socialLinks)
-	http.Handle("/", templ.Handler(homeComponent))
-	http.Handle("/photos", templ.Handler(PhotoPage(socialLinks)))
-	http.Handle("/writing", templ.Handler(page("Writing", socialLinks)))
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
-	http.Handle("/robots.txt", http.FileServer(http.Dir("./static/")))
+	app := fiber.New()
 
-	mediaResponse, err := media()
-	if err != nil {
-		log.Fatalf("Failed to get media: %v", err)
-	}
-	http.Handle("/instagram/media", templ.Handler(Photos(mediaResponse)))
+	app.Get("/", func(c *fiber.Ctx) error {
+		socialLinks := []SocialLink{
+			{name: "Github", url: "https://github.com/mattcollie", svg: "/static/images/github.svg"},
+			{name: "Linkedin", url: "https://www.linkedin.com/in/matt-collecutt", svg: "/static/images/linkedin.svg"},
+			{name: "Instagram", url: "https://www.instagram.com/matthewcollecutt", svg: "/static/images/instagram.svg"},
+		}
+		return Render(c, HomePage(socialLinks))
+	})
+	app.Get("/media", func(c *fiber.Ctx) error {
+		mediaResponse, err := media()
+		if err != nil {
+			return err
+		}
+		return Render(c, Photos(mediaResponse))
+	})
+	app.Get("/media/after/:after", func(c *fiber.Ctx) error {
+		after := c.Params("after")
+		mediaResponse, err := mediaAfter(after)
+		if err != nil {
+			return err
+		}
+		return Render(c, Photos(mediaResponse))
+	})
+	app.Get("/photos", func(c *fiber.Ctx) error {
+		socialLinks := []SocialLink{
+			{name: "Github", url: "https://github.com/mattcollie", svg: "/static/images/github.svg"},
+			{name: "Linkedin", url: "https://www.linkedin.com/in/matt-collecutt", svg: "/static/images/linkedin.svg"},
+			{name: "Instagram", url: "https://www.instagram.com/matthewcollecutt", svg: "/static/images/instagram.svg"},
+		}
+		mediaResponse, err := media()
+		if err != nil {
+			return err
+		}
+		return Render(c, PhotoPage(socialLinks, mediaResponse))
+	})
+	app.Get("/writing", func(c *fiber.Ctx) error {
+		socialLinks := []SocialLink{
+			{name: "Github", url: "https://github.com/mattcollie", svg: "/static/images/github.svg"},
+			{name: "Linkedin", url: "https://www.linkedin.com/in/matt-collecutt", svg: "/static/images/linkedin.svg"},
+			{name: "Instagram", url: "https://www.instagram.com/matthewcollecutt", svg: "/static/images/instagram.svg"},
+		}
+		return Render(c, page("Writing", socialLinks))
+	})
+	app.Get("/robots.txt", func(c *fiber.Ctx) error {
+		return c.SendFile("./static/robots.txt")
+	})
+	app.Static("/static", "./static")
 
 	addr := cfg.Server.Host + ":" + cfg.Server.Port
-	fmt.Println("Listening on " + addr)
-	err = http.ListenAndServe(addr, nil)
-	if err != nil {
-		log.Fatalf("Fatal error failed to start server: %s \\n", err)
+	log.Println("Listening on " + addr)
+	if err := app.Listen(addr); err != nil {
+		log.Fatalf("Fatal error failed to start server: %s\n", err)
 	}
 }
 
-func media() ([]MediaItem, error) {
-	url := fmt.Sprintf("https://graph.instagram.com/%s/media?fields=caption,id,media_type,media_url,timestamp&access_token=%s", cfg.Instagram.UserId, cfg.Instagram.AccessToken)
+func media() (MediaResponse, error) {
+	limit := 12
+	url := fmt.Sprintf("https://graph.instagram.com/%s/media?fields=caption,id,media_type,media_url,timestamp,children{id,media_url}&access_token=%s&limit=%d", cfg.Instagram.UserId, cfg.Instagram.AccessToken, limit)
 
 	response, err := http.Get(url)
 	if err != nil {
-		return nil, fmt.Errorf("error making request: %w", err)
+		return MediaResponse{}, fmt.Errorf("error making request: %w", err)
 	}
 
 	if response.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("API request failed with status code %d", response.StatusCode)
+		return MediaResponse{}, fmt.Errorf("API request failed with status code %d", response.StatusCode)
 	}
 	defer response.Body.Close()
 
 	var myResponse MediaResponse
 	if err = json.NewDecoder(response.Body).Decode(&myResponse); err != nil {
-		return nil, fmt.Errorf("error decoding response: %w", err)
+		return MediaResponse{}, fmt.Errorf("error decoding response: %w", err)
 	}
 
-	return myResponse.MediaItems, nil
+	return myResponse, nil
+}
+
+func mediaAfter(after string) (MediaResponse, error) {
+	limit := 12
+	url := fmt.Sprintf("https://graph.instagram.com/%s/media?fields=caption,id,media_type,media_url,timestamp,children{id,media_url}&access_token=%s&limit=%d&after=%s", cfg.Instagram.UserId, cfg.Instagram.AccessToken, limit, after)
+
+	response, err := http.Get(url)
+	if err != nil {
+		return MediaResponse{}, fmt.Errorf("error making request: %w", err)
+	}
+
+	if response.StatusCode != http.StatusOK {
+		return MediaResponse{}, fmt.Errorf("API request failed with status code %d", response.StatusCode)
+	}
+	defer response.Body.Close()
+
+	var myResponse MediaResponse
+	if err = json.NewDecoder(response.Body).Decode(&myResponse); err != nil {
+		return MediaResponse{}, fmt.Errorf("error decoding response: %w", err)
+	}
+
+	return myResponse, nil
+}
+
+func Render(c *fiber.Ctx, component templ.Component, options ...func(handler *templ.ComponentHandler)) error {
+	componentHandler := templ.Handler(component)
+	for _, o := range options {
+		o(componentHandler)
+	}
+
+	return adaptor.HTTPHandler(componentHandler)(c)
 }
