e531b4efa6537c9c9ad89da04e47409d0c2d955c
mattcollie <mattcollie@live.com>
2026-03-28 21:04:13 +1300

    chore: integrate PostHog for analytics and remove inlined init script

--- ui/portfolio/src/routes/+layout.svelte
+++ ui/portfolio/src/routes/+layout.svelte
@@ -1,7 +1,21 @@
 <script lang="ts">
 	import '../app.css';
+	import { onMount } from 'svelte';
+	import { browser } from '$app/environment';
+	import { PUBLIC_POSTHOG_KEY, PUBLIC_POSTHOG_HOST } from '$env/static/public';
 
 	let { children } = $props();
+
+	onMount(() => {
+		if (browser && PUBLIC_POSTHOG_KEY) {
+			import('posthog-js').then(({ default: posthog }) => {
+				posthog.init(PUBLIC_POSTHOG_KEY, {
+					api_host: PUBLIC_POSTHOG_HOST,
+					person_profiles: 'always'
+				});
+			});
+		}
+	});
 </script>
 
 <svelte:head>
